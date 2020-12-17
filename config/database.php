db8c32180bbd90eec472e626572552d677501ce9
Chrisphine10 <ondiekchrisphine@gmail.com>
2020-12-17 10:14:33 +0300

    con moreee

--- config/database.php
+++ config/database.php
@@ -66,6 +66,7 @@ return [
             'password' => $password,
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
+            'options' => [PDO::ATTR_EMULATE_PREPARES => true],
             'prefix' => '',
             'strict' => false,
             'engine' => null,
